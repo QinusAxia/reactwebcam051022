df003901100bb37711edfe4e206e2417788a380a
QinusAxia <36871288+QinusAxia@users.noreply.github.com>
2022-10-05 17:59:26 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Test Portal</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </React.Fragment>
+
+  )
 }
 
 export default MyApp
